639408533606ba9763428020d017f7fcfbe5bc5d
MikhailPakaliuk <mr.dyurilen@mail.ru>
2019-12-06 10:03:05 +0300

    fix: solve angleBetweenClockHands
    thoughts on how to solve the angleBetweenClockHands

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -93,31 +93,32 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5, 3, 0) => Math.PI/2
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
+ *    Date.UTC(2016,3,5,14,20) => 0.8726646259971648
  */
 
 function angleBetweenClockHands(date) {
     function modNOD(a, b) {
-        if (a == 0 || b== 0) {
+        if (a == 0 || b == 0) {
             return Math.max(a, b);
-        }else {
+        } else {
             while (a != b) {
                 a > b ? a -= b : b -= a;
             }
             return a;
         }
     }
-    let hour_hand_nod = modNOD(new Date(date).getUTCHours(), 6);
-    let minute_hand_date = new Date(date).getUTCMinutes() * Math.PI / 30;
-
-    let hour_hand_date = (function (params) {
+    let nod_hour_hand = modNOD(new Date(date).getUTCHours(), 6);
+    let nod_minute_hand = modNOD(new Date(date).getUTCMinutes(), 30);
+    let minute_hand_date = ((new Date(date).getUTCMinutes()/nod_minute_hand) * Math.PI.toFixed(15) / (30/nod_minute_hand));
+    let incremental_hour_hand_date = (Math.PI / 6).toFixed(16) * (new Date(date).getUTCMinutes() / 60);
+    let hour_hand_date = incremental_hour_hand_date + (function (params) {
         while (params > Math.PI) {
             params -= Math.PI;
         }
         return params;
-    })((new Date(date).getUTCHours()/hour_hand_nod) * Math.PI / (6/hour_hand_nod));
-
-    const noon = 12 * 60;
-    /*if( minutes_date>=noon) minutes_date-=noon:
+    })((new Date(date).getUTCHours() / nod_hour_hand) * Math.PI / (6 / nod_hour_hand));
+    /*const noon = 12 * 60;
+    if( minutes_date>=noon) minutes_date-=noon:
     if(minutes_date>noon/2) minutes_date=noon-minutes_date;
     }*/
     return Math.abs(hour_hand_date - minute_hand_date);
